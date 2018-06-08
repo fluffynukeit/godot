8cfab3d86797f659014b2fb09f56856c4654ea29
Andrea Catania <info@andreacatania.com>
2018-06-08 09:37:03 +0200

    Renamed ParticleShape to ParticleBodyModel to avoid next feature name conflict

--- scene/register_scene_types.cpp
+++ scene/register_scene_types.cpp
@@ -146,6 +146,7 @@
 #include "scene/resources/mesh_data_tool.h"
 #include "scene/resources/mesh_library.h"
 #include "scene/resources/packed_scene.h"
+#include "scene/resources/particle_body_model.h"
 #include "scene/resources/particle_shape.h"
 #include "scene/resources/particles_material.h"
 #include "scene/resources/physics_material.h"
@@ -600,6 +601,7 @@ void register_scene_types() {
 	ClassDB::register_class<ConcavePolygonShape>();
 
 	ClassDB::register_class<ParticleShape>();
+	ClassDB::register_class<ParticleBodyModel>();
 
 	ClassDB::register_class<SurfaceTool>();
 	ClassDB::register_class<MeshDataTool>();
