74649d983ac36b14b0480dabf940a2d78753541c
Andrea Catania <info@andreacatania.com>
2018-06-27 19:12:04 +0200

    Implementing interface to create soft rigid cloth

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -100,6 +100,7 @@
 #include "editor/plugins/path_2d_editor_plugin.h"
 #include "editor/plugins/path_editor_plugin.h"
 #include "editor/plugins/physical_bone_plugin.h"
+#include "editor/plugins/physics_particle_body_editor_plugin.h"
 #include "editor/plugins/polygon_2d_editor_plugin.h"
 #include "editor/plugins/resource_preloader_editor_plugin.h"
 #include "editor/plugins/root_motion_editor_plugin.h"
@@ -5918,6 +5919,7 @@ EditorNode::EditorNode() {
 	add_editor_plugin(memnew(PhysicalBonePlugin(this)));
 	add_editor_plugin(memnew(MeshEditorPlugin(this)));
 	add_editor_plugin(memnew(MaterialEditorPlugin(this)));
+	add_editor_plugin(memnew(PhysicsParticleBodyEditorPlugin(this)));
 
 	for (int i = 0; i < EditorPlugins::get_plugin_count(); i++)
 		add_editor_plugin(EditorPlugins::create(i, this));
