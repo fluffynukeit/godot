57247b483a037cadc6f5fd924a205c072412d613
Andrea Catania <info@andreacatania.com>
2019-04-13 13:07:45 +0200

    Correctly added uniform brain area

--- modules/brain/register_types.cpp
+++ modules/brain/register_types.cpp
@@ -29,8 +29,9 @@ void print_error_callback(
 }
 
 void register_brain_types() {
-	ClassDB::register_class<BrainArea>();
 	ClassDB::register_class<SynapticTerminals>();
+	ClassDB::register_class<BrainArea>();
+	ClassDB::register_class<UniformBrainArea>();
 
 	error_handler = new brain::ErrorHandlerList;
 	error_handler->errfunc = print_error_callback;
